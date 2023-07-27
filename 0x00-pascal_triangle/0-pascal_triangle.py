487c54b1663f66422bb657937b87bbcce44ec061
Chikaoiri Agu <agu.chikaodiri.ng@gmail.com>
2023-07-27 14:53:19 +0100

    Changes saved

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,24 @@
+#!/usr/bin/python3
+"""
+0. Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            a = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                a = a * (i - j) // j
+            res.append(level)
+    return res
+"""
 def pascal_triangle(n):
     a = [[] for i in range(n)]
     for i in range(n):
@@ -13,3 +34,4 @@ def pascal_triangle(n):
     return a
 n = 5
 print(pascal_triangle(n))
+"""
